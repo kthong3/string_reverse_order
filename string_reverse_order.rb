a8ed1dd9ee05e55221634026a132c2808b4b0c1a
kthong3 <kthongsakounh@gmail.com>
2018-04-06 11:47:27 -0500

    swap split string index

--- string_reverse_order.rb
+++ string_reverse_order.rb
@@ -11,15 +11,28 @@ sentence = "what do you mean?"
   # swap first with last word until loop meets the middle of the string
 
 # to copy
+# def reverse_sentence(sentence)
+#   split = sentence.split(" ")
+#   reversed = []
+#   i = split.length - 1
+#   until i < 0
+#     reversed << split[i]
+#     i -= 1
+#   end
+#   reversed.join(" ")
+# end
+
+# swap in place
 def reverse_sentence(sentence)
   split = sentence.split(" ")
-  reversed = []
-  i = split.length - 1
-  until i < 0
-    reversed << split[i]
-    i -= 1
+  i = 0
+  j = split.length - 1
+  while i < split.length / 2
+    split[i], split[j] = split[j], split[i]
+    i += 1
+    j -= 1
   end
-  reversed.join(" ")
+  reversed = split.join(" ")
 end
 
 reverse_sentence(sentence)
