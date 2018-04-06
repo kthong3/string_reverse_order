853a754a6355d78d36c69d789b326de4fb4f1da8
kthong3 <kthongsakounh@gmail.com>
2018-04-06 11:34:12 -0500

    create reverse method with copying string

--- string_reverse_order.rb
+++ string_reverse_order.rb
@@ -10,6 +10,16 @@ sentence = "what do you mean?"
 # for each word
   # swap first with last word until loop meets the middle of the string
 
+# to copy
 def reverse_sentence(sentence)
-
+  split = sentence.split(" ")
+  reversed = []
+  i = split.length - 1
+  until i < 0
+    reversed << split[i]
+    i -= 1
+  end
+  reversed.join(" ")
 end
+
+reverse_sentence(sentence)
